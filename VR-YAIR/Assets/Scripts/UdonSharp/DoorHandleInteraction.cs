1e2a6ecf2c8ebf5adb216903973bb8083e10ceb0
AngelVelascoJr <80130331+AngelVelascoJr@users.noreply.github.com>
2024-02-08 13:32:30 -0600

    Agregado de Puerta y Modelo a escena principal

---
+++ VR-YAIR/Assets/Scripts/UdonSharp/DoorHandleInteraction.cs
@@ -0,0 +1,79 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DoorHandleInteraction : UdonSharpBehaviour
+{
+    [SerializeField] VRC_Pickup _Pickup;
+    //[SerializeField] Transform[] NonvrDoorPosition;
+    [SerializeField] Vector3[] DoorRotation;
+    [SerializeField] Transform Door;
+    [SerializeField] GameObject GrabbableHandle;
+    [SerializeField] Transform HandleDefaultPosition;
+    bool IsDoorOpened;
+    bool VRUserHold;
+
+    private void Awake()
+    {
+        //_Pickup = (VRC_Pickup)GetComponent(typeof(VRC_Pickup));
+        //GrabbableHandle = gameObject;
+    }
+
+    private void Update()
+    {
+        //if (VRUserHold)
+        //{
+            Vector3 dir = new Vector3(gameObject.transform.position.x, Door.transform.position.y, gameObject.gameObject.transform.position.z);
+            Door.transform.LookAt(dir);
+        //}
+    }
+
+    public override void OnPickup()
+    {
+        //Si es VR, entonces nada
+        //si no es VR, entonces mueves el objeto a la nueva posicion
+        VRUserHold = _Pickup.currentPlayer.IsUserInVR();
+        if (VRUserHold)
+        {
+
+        }
+        else
+        {
+            IsDoorOpened = !IsDoorOpened;
+            Door.localRotation = Quaternion.Euler(DoorRotation[IsDoorOpened ? 1 : 0]);
+            //Debug.Log(Door.localRotation.eulerAngles);
+            //Door.LookAt(-NonvrDoorPosition[IsDoorOpened ? 1 : 0].position);
+        }
+    }
+
+    public override void OnDrop()
+    {
+        //Si es VR, entonces calcular si esta mas cerca de abrir o cerrar
+        //En cualquier caso, mueves el handle nuevamente a su posicion
+        float IsClosedLenght = Vector3.Distance(Door.transform.localRotation.eulerAngles, DoorRotation[0]);//Vector3.Distance(HandleDefaultPosition.position, NonvrDoorPosition[0].position);
+        //Debug.Log(Door.transform.localRotation.eulerAngles);
+        //Debug.Log(DoorRotation[0]);
+        float IsOpenedLenght = Vector3.Distance(Door.transform.localRotation.eulerAngles, DoorRotation[1]);//Vector3.Distance(HandleDefaultPosition.position, NonvrDoorPosition[1].position);
+        //Debug.Log(Door.transform.localRotation.eulerAngles);
+        //Debug.Log(DoorRotation[1]);
+        if (IsClosedLenght < IsOpenedLenght)
+        {
+            IsDoorOpened = false;
+            //Debug.Log("Closed");
+        }
+        else
+        {
+            IsDoorOpened = true;
+            //Debug.Log("Opened");
+        }
+        //Debug.Log("IsClossed: " + IsClosedLenght + ", IsOppened: " + IsOpenedLenght);
+        //if (!VRUserHold)
+        //{        
+        //    IsDoorOpened = !IsDoorOpened;
+        //}
+        VRUserHold = false;
+        GrabbableHandle.transform.position = HandleDefaultPosition.transform.position;
+    }
+
+}
--- VR-YAIR/Assets/Scripts/UdonSharp/DoorInteraction.cs
+++ VR-YAIR/Assets/Scripts/UdonSharp/DoorInteraction.cs
@@ -9,29 +9,48 @@ public class DoorInteraction : UdonSharpBehaviour
 {
     [SerializeField] VRC_Pickup _Pickup;
     [SerializeField] Rigidbody _RigidbodyDoor;
-    [SerializeField] GameObject LookatObj;
+    [SerializeField] GameObject Door;
     [SerializeField] GameObject ObjectToLookAt;
     [SerializeField] GameObject Manija;
+    [SerializeField] GameObject[] NonVrPositions;
+    [SerializeField] bool isOpened = false;
+    bool updateDoorState;
     [SerializeField] float AddedForce;
-    bool Holds = false;
+    bool VRHolds = false;
     public override void OnPickup()
     {
         if(_Pickup.currentPlayer.IsUserInVR())
         {
-            Holds = true;
+            VRHolds = true;
         }
         else
         {
-            _RigidbodyDoor.AddForce((gameObject.transform.position - _Pickup.currentPlayer.GetPosition()).normalized * AddedForce);
+            isOpened = !isOpened;
+            updateDoorState = true;
+            //_RigidbodyDoor.AddForce((gameObject.transform.position - _Pickup.currentPlayer.GetPosition()).normalized * AddedForce);
         }
     }
 
     private void Update()
     {
-        if(Holds)
+        if(updateDoorState)
         {
-            Vector3 dir = new Vector3(gameObject.transform.position.x, LookatObj.transform.position.y, gameObject.gameObject.transform.position.z);
-            LookatObj.transform.LookAt(dir);
+            if (isOpened)
+            {
+                Vector3 dir = new Vector3(NonVrPositions[0].transform.position.x, Door.transform.position.y, NonVrPositions[0].gameObject.transform.position.z);
+                Door.transform.LookAt(dir);
+            }
+            else
+            {
+                Vector3 dir = new Vector3(NonVrPositions[1].transform.position.x, Door.transform.position.y, NonVrPositions[1].gameObject.transform.position.z);
+                Door.transform.LookAt(dir);
+            }
+            updateDoorState = false;
+        }
+        if (VRHolds)
+        {
+            Vector3 dir = new Vector3(gameObject.transform.position.x, Door.transform.position.y, gameObject.gameObject.transform.position.z);
+            Door.transform.LookAt(dir);
         }
         else
         {
@@ -41,6 +60,6 @@ public class DoorInteraction : UdonSharpBehaviour
 
     public override void OnDrop()
     {
-        Holds = false;
+        VRHolds = false;
     }
 }
