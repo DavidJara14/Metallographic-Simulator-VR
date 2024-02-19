11b8972c76795dd554f34e6b8483986bc2c1a10b
AngelVelascoJr <80130331+AngelVelascoJr@users.noreply.github.com>
2024-02-19 09:51:15 -0600

    lampara, iluminacion y papitas

---
+++ VR-YAIR/Assets/Scripts/UdonSharp/Cheto.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Cheto : UdonSharpBehaviour
+{
+    [SerializeField] private Transform _ref;
+    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private AudioSource _audioSource;
+
+    public override void OnDrop()
+    {
+        gameObject.transform.position = _ref.position;
+        _particleSystem.Emit(1);
+        _audioSource.Play();
+    }
+}
