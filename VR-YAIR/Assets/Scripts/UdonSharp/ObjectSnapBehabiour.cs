6255894220c520cade139922260e4a29f8691881
AngelVelascoJr <80130331+AngelVelascoJr@users.noreply.github.com>
2024-03-07 12:28:59 -0600

    AdddSnapPreview

--- VR-YAIR/Assets/Scripts/UdonSharp/ObjectSnapBehabiour.cs
+++ VR-YAIR/Assets/Scripts/UdonSharp/ObjectSnapBehabiour.cs
@@ -1,12 +1,22 @@
 ﻿
 using UdonSharp;
+using UdonSharpEditor;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
 public class ObjectSnapBehabiour : UdonSharpBehaviour
 {
+    [SerializeField] bool SnapPosition;
+    [SerializeField] bool SnapRotation;
 
-    
+    public void OnDropCustom()
+    {
+        GameObject var = this.gameObject;
+        if(var.GetComponentInParent<ObjectSnapBehabiour>() != null)//Checar por layer Pickup, con un spherecast
+        {
+
+        }
+    }
 
 }
