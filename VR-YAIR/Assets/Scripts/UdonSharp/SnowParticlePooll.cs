71c4aba01ccc99b66f37146d8ababbafc819a2ad
lorpc05 <quest2561980@gmail.com>
2024-06-11 13:59:18 -0600

    puntosinretornoxd

---
+++ VR-YAIR/Assets/Scripts/UdonSharp/SnowParticlePooll.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SnowParticlePooll : UdonSharpBehaviour
+{
+
+    public VRCObjectPool pool;
+
+    private void Awake()
+    {
+        if(pool == null)
+            pool = GetComponent<VRCObjectPool>();
+    }
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        GameObject go = pool.TryToSpawn();
+        if(go != null)
+        {
+            go.GetComponent<SnowParticleFollowPlayer>().target = player;
+        }
+        else
+        {
+            Debug.Log("Mas");
+        }
+    }
+
+}
