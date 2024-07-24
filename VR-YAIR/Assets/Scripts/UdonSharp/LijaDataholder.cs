6418422c12e946b06380ef959db63dabd0318e8d
lorpc05 <quest2561980@gmail.com>
2024-07-24 12:33:30 -0600

    Salvacion3

---
+++ VR-YAIR/Assets/Scripts/UdonSharp/LijaDataholder.cs
@@ -0,0 +1,31 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDK3.Data;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class LijaDataholder : UdonSharpBehaviour
+{
+    public VRCObjectPool LijaPool;
+
+    public Material[] MaterialesSegunTamañosDeLija;
+
+    public DataDictionary LijaDict = new DataDictionary()
+    {
+        {120, 0},
+        {180, 1},
+        {240, 2},
+        {360, 3},
+        {400, 4},
+        {500, 5},
+        {600, 6},
+        {800, 7},
+    };
+
+    private void Start()
+    {
+        Debug.Log(MaterialesSegunTamañosDeLija.Length);
+    }
+
+}
