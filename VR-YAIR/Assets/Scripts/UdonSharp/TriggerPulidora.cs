3b0dea5eaaf200a48b9480cafc1659726ab0a748
AngelVelascoJr <80130331+AngelVelascoJr@users.noreply.github.com>
2025-01-14 16:38:41 -0600

    add shrink to colider in pulidora

--- VR-YAIR/Assets/Scripts/UdonSharp/TriggerPulidora.cs
+++ VR-YAIR/Assets/Scripts/UdonSharp/TriggerPulidora.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Threading;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -7,4 +8,56 @@ using VRC.Udon;
 public class TriggerPulidora : UdonSharpBehaviour
 {
 
+    const float TIMER_MAX = 1f;
+    float ResetTimer = TIMER_MAX;
+    bool startTimer = false;
+
+    [SerializeField] GameObject coliderToShrink;
+    Vector3 originalSize;
+
+    private void Start()
+    {
+        originalSize = coliderToShrink.transform.localScale;
+    }
+
+    private void Update()
+    {
+        if(startTimer)
+        {
+            ResetTimer -= Time.deltaTime;
+            if(ResetTimer < 0 )
+            {
+                ResetTimer = TIMER_MAX;
+                startTimer = false;
+                coliderToShrink.transform.localScale = originalSize;
+                Debug.LogWarning("Originalited");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<ActivateMirror>() == null)
+            return;
+        if (other.GetComponent<VRC_Pickup>().currentPlayer != null)
+        {
+            //Debug.Log("Hay Player Agarrando");
+            if (!Networking.IsOwner(Networking.LocalPlayer, this.gameObject))
+            {
+                Debug.Log("el player no es local, regresando");
+                return;
+            }
+            //Debug.Log("LocalPlayer");
+        }
+
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Shrink");
+        //Shrink();
+    }
+
+    private void Shrink()
+    {
+        startTimer = true;
+        coliderToShrink.transform.localScale = new Vector3(0.0001f, 0.0001f, 0.0001f);
+        Debug.LogWarning("shrinked");
+    }
 }
