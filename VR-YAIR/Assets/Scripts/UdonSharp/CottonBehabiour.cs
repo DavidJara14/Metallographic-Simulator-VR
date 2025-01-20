7e150922616d456e643fcdcf2743040446609543
DavidJara14 <153585940+DavidJara14@users.noreply.github.com>
2025-01-20 01:25:00 -0600

    Added cotton behabiour script

---
+++ VR-YAIR/Assets/Scripts/UdonSharp/CottonBehabiour.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CottonBehabiour : UdonSharpBehaviour
+{
+    [UdonSynced] [SerializeField] public bool haveAlcohol = false;
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.GetComponentInParent<BotellaLab>() != null)
+        {
+            string tipo = other.GetComponentInParent<BotellaLab>().Tipo;
+            if (tipo == "Alcohol")
+            {
+                haveAlcohol = true;
+            }
+        }
+    }
+}
