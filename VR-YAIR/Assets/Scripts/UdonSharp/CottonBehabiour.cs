0b9294f9375f343b9a8c4675dd29de4ba26d4ca3
AngelVelascoJr <80130331+AngelVelascoJr@users.noreply.github.com>
2025-01-30 16:39:51 -0600

    add cotton effects

--- VR-YAIR/Assets/Scripts/UdonSharp/CottonBehabiour.cs
+++ VR-YAIR/Assets/Scripts/UdonSharp/CottonBehabiour.cs
@@ -7,4 +7,18 @@ using VRC.Udon;
 public class CottonBehabiour : UdonSharpBehaviour
 {
     //Clase etiqueta, no borrar
+
+    [SerializeField] Material cottonMat;
+
+    private void Start()
+    {
+        cottonMat = GetComponentInParent<MeshRenderer>().material;
+    }
+
+    public void AddAlcohol()
+    {
+        cottonMat.SetFloat("_AditionalPower", 3.5f);
+        cottonMat.SetInteger("_HasAlcohol", 1);
+    }
+
 }
