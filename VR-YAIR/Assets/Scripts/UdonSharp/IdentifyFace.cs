94ff24b5f6f1575e459bd237d7159f2b4d7faf79
DavidJara14 <153585940+DavidJara14@users.noreply.github.com>
2025-04-14 18:45:49 -0600

    Added IdentifyFace Script

---
+++ VR-YAIR/Assets/Scripts/UdonSharp/IdentifyFace.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class IdentifyFace : UdonSharpBehaviour
+{
+    [SerializeField] private GameObject myShader;
+    [SerializeField] private GameObject myMirror;
+    [SerializeField] private ProbeBehabiour probeBehabiour;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.LogWarning("Face: " + gameObject.name + " Collision with: " + other.gameObject.name);
+        if(other.gameObject.GetComponent<LijaCircularBehabiour>() || other.gameObject.GetComponent<PulidoraScript>())
+        {
+            probeBehabiour.probetaShader = myShader;
+            probeBehabiour.Desgaste = myShader.GetComponent<Renderer>().material.GetFloat("_GranoLija");
+        }
+    }
+}
