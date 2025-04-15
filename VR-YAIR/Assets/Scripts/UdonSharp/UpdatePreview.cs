ed9ace297f8c6de28fff41c21f74b5f62654fc51
DavidJara14 <153585940+DavidJara14@users.noreply.github.com>
2025-04-14 19:03:08 -0600

    Update UpdatePreview script

--- VR-YAIR/Assets/Scripts/UdonSharp/UpdatePreview.cs
+++ VR-YAIR/Assets/Scripts/UdonSharp/UpdatePreview.cs
@@ -6,20 +6,90 @@ using VRC.Udon;
 
 public class UpdatePreview : UdonSharpBehaviour
 {
-    public GameObject probetaShaderParent;
-    public GameObject probetaMirrorParent;
-
+    [Header("Faces Probeta")]
+    public GameObject probetaShaderParent_inf;
+    public GameObject probetaMirrorParent_inf;
+    public GameObject probetaShaderParent_sup;
+    public GameObject probetaMirrorParent_sup;
+    
+    [Header("Faces Canva")]
     public GameObject probetaShaderChildren;
     public GameObject probetaMirrorChildren;
 
+
+    [SerializeField] public BorderColor borderColor;
+
+
+    [Header("RenderInfo")]
+
+    [SerializeField] private float reflexion = 0;
+    [SerializeField] private float granoLija = 0;
+    [SerializeField] private int isFirstSanding = 0;
+    [SerializeField] private float angleRotation = 0;
+
+    [SerializeField] private float timer = 0f;
+    [SerializeField] private bool switchFlag = false;
+
+    private void Start()
+    {
+        probetaShaderChildren.SetActive(probetaShaderParent_inf.activeSelf);
+        probetaMirrorChildren.SetActive(probetaMirrorParent_inf.activeSelf);
+    }
     private void Update()
     {
-        probetaShaderChildren.SetActive(probetaShaderParent.activeSelf);
-        probetaMirrorChildren.SetActive(probetaMirrorParent.activeSelf);
+        probetaShaderChildren.GetComponent<Renderer>().material.SetFloat("_Reflexion", reflexion);
+        probetaShaderChildren.GetComponent<Renderer>().material.SetFloat("_GranoLija", granoLija);
+        probetaShaderChildren.GetComponent<Renderer>().material.SetInt("_IsFirstSanding", isFirstSanding);
+        probetaShaderChildren.GetComponent<Renderer>().material.SetFloat("_AngleRotation", angleRotation);
 
-        probetaShaderChildren.GetComponent<Renderer>().material.SetFloat("_Reflexion", probetaShaderParent.GetComponent<Renderer>().material.GetFloat("_Reflexion"));
-        probetaShaderChildren.GetComponent<Renderer>().material.SetFloat("_GranoLija", probetaShaderParent.GetComponent<Renderer>().material.GetFloat("_GranoLija"));
-        probetaShaderChildren.GetComponent<Renderer>().material.SetInt("_IsFirstSanding", probetaShaderParent.GetComponent<Renderer>().material.GetInt("_IsFirstSanding"));
-        probetaShaderChildren.GetComponent<Renderer>().material.SetFloat("_AngleRotation", probetaShaderParent.GetComponent<Renderer>().material.GetFloat("_AngleRotation"));
+        if (switchFlag && timer<1.5f)
+        {
+            borderColor.SendCustomEvent("colorYellow");
+            timer += Time.deltaTime;
+        }
+
+        if(timer > 1f)
+        {
+            switchFlag = false;
+            timer = 0f;
+        }
+
+        if(!switchFlag)
+        {
+            probetaShaderParent_inf.GetComponent<Renderer>().materials[1].SetFloat("_Scale", 0f);
+            probetaShaderParent_sup.GetComponent<Renderer>().materials[1].SetFloat("_Scale", 0f);
+        }
+    }
+
+    public void switchFaces_Off()
+    {
+        probetaShaderChildren.SetActive(probetaShaderParent_inf.activeSelf);
+        probetaMirrorChildren.SetActive(probetaMirrorParent_inf.activeSelf);
+        probetaShaderParent_sup.GetComponent<Renderer>().materials[1].SetFloat("_Scale", 0f);
+        borderColor.bodyMaterial = probetaShaderParent_inf;
+        getDataFaces(probetaShaderParent_inf);
+        switchFlag = true;
+        timer = 0f;
+        Debug.LogWarning("[<color=blue>Change to face inferior</color>]");
+    }
+
+    public void switchFaces_On()
+    {
+        probetaShaderChildren.SetActive(probetaShaderParent_sup.activeSelf);
+        probetaMirrorChildren.SetActive(probetaMirrorParent_sup.activeSelf);
+        probetaShaderParent_inf.GetComponent<Renderer>().materials[1].SetFloat("_Scale", 0f);
+        borderColor.bodyMaterial = probetaShaderParent_sup;
+        getDataFaces(probetaShaderParent_sup);
+        switchFlag = true;
+        timer = 0f;
+        Debug.LogWarning("[<color=blue>Change to face superior</color>]");
+    }
+
+    private void getDataFaces(GameObject thisFace)
+    {
+        reflexion = thisFace.GetComponent<Renderer>().material.GetFloat("_Reflexion");
+        granoLija = thisFace.GetComponent<Renderer>().material.GetFloat("_GranoLija");
+        isFirstSanding = thisFace.GetComponent<Renderer>().material.GetInt("_IsFirstSanding");
+        angleRotation = thisFace.GetComponent<Renderer>().material.GetFloat("_AngleRotation");
     }
 }
