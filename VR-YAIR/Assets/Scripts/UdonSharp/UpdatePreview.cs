a0fa615f4ac3d28620005e96a4c55b5c73ef49de
DavidJara14 <153585940+DavidJara14@users.noreply.github.com>
2025-04-17 16:13:25 -0600

    Update UpdatePreview

--- VR-YAIR/Assets/Scripts/UdonSharp/UpdatePreview.cs
+++ VR-YAIR/Assets/Scripts/UdonSharp/UpdatePreview.cs
@@ -1,6 +1,7 @@
 ﻿
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.UIElements;
 using VRC.SDKBase;
 using VRC.Udon;
 
@@ -15,13 +16,13 @@ public class UpdatePreview : UdonSharpBehaviour
     [Header("Faces Canva")]
     public GameObject probetaShaderChildren;
     public GameObject probetaMirrorChildren;
-
+    [SerializeField] private GameObject probetaShaderActual;
 
     [SerializeField] public BorderColor borderColor;
+    [SerializeField] private bool supIsActive = false;
 
 
     [Header("RenderInfo")]
-
     [SerializeField] private float reflexion = 0;
     [SerializeField] private float granoLija = 0;
     [SerializeField] private int isFirstSanding = 0;
@@ -32,15 +33,22 @@ public class UpdatePreview : UdonSharpBehaviour
 
     private void Start()
     {
-        probetaShaderChildren.SetActive(probetaShaderParent_inf.activeSelf);
-        probetaMirrorChildren.SetActive(probetaMirrorParent_inf.activeSelf);
+        activeSelf(probetaShaderParent_inf, probetaMirrorParent_inf);
+        probetaShaderActual = probetaShaderParent_inf;
     }
     private void Update()
     {
-        probetaShaderChildren.GetComponent<Renderer>().material.SetFloat("_Reflexion", reflexion);
-        probetaShaderChildren.GetComponent<Renderer>().material.SetFloat("_GranoLija", granoLija);
-        probetaShaderChildren.GetComponent<Renderer>().material.SetInt("_IsFirstSanding", isFirstSanding);
-        probetaShaderChildren.GetComponent<Renderer>().material.SetFloat("_AngleRotation", angleRotation);
+        updateShader(probetaShaderChildren);
+
+        if (supIsActive)
+        {
+            activeSelf(probetaShaderParent_sup, probetaMirrorParent_sup);
+        }
+        if (!supIsActive)
+        {
+            activeSelf(probetaShaderParent_inf, probetaMirrorParent_inf);
+        }
+
 
         if (switchFlag && timer<1.5f)
         {
@@ -56,18 +64,23 @@ public class UpdatePreview : UdonSharpBehaviour
 
         if(!switchFlag)
         {
-            probetaShaderParent_inf.GetComponent<Renderer>().materials[1].SetFloat("_Scale", 0f);
-            probetaShaderParent_sup.GetComponent<Renderer>().materials[1].SetFloat("_Scale", 0f);
+            scaleAllMaterial();
+
+            if (probetaMirrorChildren.activeSelf)
+            {
+                if (supIsActive) { probetaShaderParent_sup.SetActive(false); }
+                if (!supIsActive) { probetaShaderParent_inf.SetActive(false); }
+            }
         }
     }
 
     public void switchFaces_Off()
     {
-        probetaShaderChildren.SetActive(probetaShaderParent_inf.activeSelf);
-        probetaMirrorChildren.SetActive(probetaMirrorParent_inf.activeSelf);
-        probetaShaderParent_sup.GetComponent<Renderer>().materials[1].SetFloat("_Scale", 0f);
+        forceActiveShader(probetaShaderParent_inf, probetaMirrorParent_inf);
+        scaleShader(probetaShaderParent_sup, 0f, 1);
         borderColor.bodyMaterial = probetaShaderParent_inf;
-        getDataFaces(probetaShaderParent_inf);
+        probetaShaderActual = probetaShaderParent_inf;
+        supIsActive = false;
         switchFlag = true;
         timer = 0f;
         Debug.LogWarning("[<color=blue>Change to face inferior</color>]");
@@ -75,11 +88,11 @@ public class UpdatePreview : UdonSharpBehaviour
 
     public void switchFaces_On()
     {
-        probetaShaderChildren.SetActive(probetaShaderParent_sup.activeSelf);
-        probetaMirrorChildren.SetActive(probetaMirrorParent_sup.activeSelf);
-        probetaShaderParent_inf.GetComponent<Renderer>().materials[1].SetFloat("_Scale", 0f);
+        forceActiveShader(probetaShaderParent_sup, probetaMirrorParent_sup);
+        scaleShader(probetaShaderParent_inf, 0f, 1);
         borderColor.bodyMaterial = probetaShaderParent_sup;
-        getDataFaces(probetaShaderParent_sup);
+        probetaShaderActual = probetaShaderParent_sup;
+        supIsActive = true;
         switchFlag = true;
         timer = 0f;
         Debug.LogWarning("[<color=blue>Change to face superior</color>]");
@@ -91,5 +104,43 @@ public class UpdatePreview : UdonSharpBehaviour
         granoLija = thisFace.GetComponent<Renderer>().material.GetFloat("_GranoLija");
         isFirstSanding = thisFace.GetComponent<Renderer>().material.GetInt("_IsFirstSanding");
         angleRotation = thisFace.GetComponent<Renderer>().material.GetFloat("_AngleRotation");
+    }
+
+    private void activeSelf(GameObject shader, GameObject mirror)
+    {
+        bool thisBool = false;
+        thisBool = shader.activeSelf;
+        probetaMirrorChildren.SetActive(mirror.activeSelf);
+        if (probetaMirrorChildren.activeSelf)
+            thisBool = false;
+        probetaShaderChildren.SetActive(thisBool);
+    }
+
+    private void updateShader(GameObject shader)
+    {
+        getDataFaces(probetaShaderActual);
+        shader.GetComponent<Renderer>().material.SetFloat("_Reflexion", reflexion);
+        shader.GetComponent<Renderer>().material.SetFloat("_GranoLija", granoLija);
+        shader.GetComponent<Renderer>().material.SetInt("_IsFirstSanding", isFirstSanding);
+        shader.GetComponent<Renderer>().material.SetFloat("_AngleRotation", angleRotation);
+    }
+
+    private void scaleShader(GameObject shadertoScale, float scale, int posArray)
+    {
+        shadertoScale.GetComponent<Renderer>().materials[posArray].SetFloat("_Scale", scale);
+    }
+
+    private void forceActiveShader(GameObject shaderToActivate, GameObject mirrorToCheck)
+    {
+        if(!mirrorToCheck.activeSelf)
+            return;
+        else
+            shaderToActivate.SetActive(true);
+    }
+
+    public void scaleAllMaterial()
+    {
+        scaleShader(probetaShaderParent_inf, 0f, 1);
+        scaleShader(probetaShaderParent_sup, 0f, 1);
     }
 }
