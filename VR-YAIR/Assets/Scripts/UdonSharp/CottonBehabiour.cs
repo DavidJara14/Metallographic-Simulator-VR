b63decb619e5b8fa051ad940964dd9351ceac5df
AngelVelascoJr <80130331+AngelVelascoJr@users.noreply.github.com>
2026-03-18 11:35:25 -0600

    add a lot of things
    added new test scene
    upgrade algodon
    Update ligthing config
    update model ligth import settings

--- VR-YAIR/Assets/Scripts/UdonSharp/CottonBehabiour.cs
+++ VR-YAIR/Assets/Scripts/UdonSharp/CottonBehabiour.cs
@@ -6,19 +6,36 @@ using VRC.Udon;
 
 public class CottonBehabiour : UdonSharpBehaviour
 {
-    //Clase etiqueta, no borrar
 
+    [SerializeField] CottonTimer cottonTimer;
     [SerializeField] Material cottonMat;
+    [SerializeField] private bool hasAlcohol;
+
+    private const float AP_NA = 0.1f;
+    private const float HA_NA = 0f;
+    private const float AP_A = 3.5f;
+    private const float HA_A = 1.0f;
 
     private void Start()
     {
         cottonMat = GetComponentInParent<MeshRenderer>().material;
+        cottonTimer = GetComponent<CottonTimer>();
     }
 
     public void AddAlcohol()
     {
-        cottonMat.SetFloat("_AditionalPower", 3.5f);
-        cottonMat.SetFloat("_HasAlcohol", 1); // https://www.reddit.com/r/Unity3D/comments/wgmias/property_rect_already_exists_in_the_property/?rdt=53009
+        if(hasAlcohol)
+            return;
+        hasAlcohol = true;
+        cottonTimer.enabled = true;
+        //cottonMat.SetFloat("_AditionalPower", AP_A);
+        //cottonMat.SetFloat("_HasAlcohol", 1); // https://www.reddit.com/r/Unity3D/comments/wgmias/property_rect_already_exists_in_the_property/?rdt=53009
+    }
+
+    public void ChangeAlcohol(float value)
+    {
+        cottonMat.SetFloat("_AditionalPower", value * AP_A);
+        cottonMat.SetFloat("_HasAlcohol", value * HA_A); // https://www.reddit.com/r/Unity3D/comments/wgmias/property_rect_already_exists_in_the_property/?rdt=53009
     }
 
 }
---
+++ VR-YAIR/Assets/Scripts/UdonSharp/CottonTimer.cs
@@ -0,0 +1,34 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CottonTimer : UdonSharpBehaviour
+{
+    [SerializeField] private CottonBehabiour cottonBehabiour;
+    [SerializeField] private float Timer = 0f;
+
+    void Start()
+    {
+        cottonBehabiour = GetComponent<CottonBehabiour>();
+        this.enabled = false;
+    }
+
+    private void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer >= 1f)
+        {
+            Timer = 1f;
+            this.enabled = false;
+        }
+        cottonBehabiour.ChangeAlcohol(Timer);
+    }
+
+    private void OnEnable()
+    {
+        Timer = 0f;
+    }
+
+}
